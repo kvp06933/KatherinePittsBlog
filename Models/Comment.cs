7eee05ba2d4d911637b43c9889f28ad95f354999
Katherine Pitts <kvp06933@gmail.com>
2021-09-19 00:44:23 -0500

    Added Models with Navigation
    Added Blog, Post, Comment and Tag models with navigation properties between child and parent

---
+++ Models/Comment.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KatherinePittsBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string BlogUserId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        public string ModeratedBody { get; set; }
+
+        //Navigation Properties
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser BlogUser { get; set; }
+        public virtual IdentityUser Moderator { get; set; }
+
+    }
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KatherinePittsBlog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string BlogUserId { get; set; }
+        
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be between {2} and {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Text { get; set; }
+
+        //Navigation Properties
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser BlogUser { get; set; }
+       
+    }
+}
